5d9d93200880e35bee803529c7fa82047288ea5e
Matin Amin Sabouri <matinaminsabouri70@gmail.com>
2023-06-12 22:06:36 +0330

    (Api manager and testing REST with server)

---
+++ messenger1/apimanager.h
@@ -0,0 +1,29 @@
+#ifndef APIMANAGER_H
+#define APIMANAGER_H
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QString>
+
+class APIManager : public QObject
+{
+
+    Q_OBJECT
+
+public:
+    explicit APIManager(QObject *parent = nullptr);
+
+public slots:
+    void signUp(const QString &username, const QString &password);
+
+private slots:
+    void onReplyFinished(QNetworkReply *reply);
+
+private:
+    QNetworkAccessManager m_networkManager;
+
+};
+
+#endif // APIMANAGER_H
---
+++ messenger1/main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include "apimanager.h"
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    APIManager apiManager;
+    apiManager.signUp("matiiiiiiinTEST", "1234");
+    return a.exec();
+}
