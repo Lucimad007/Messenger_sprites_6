1c88e59e5f57a4f2e6d2a1fec8ca5375076dbc12
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-06-12 21:27:09 +0330

    done with GET , SEND and testing the server

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-
-#include <QCoreApplication>
-#include <QNetworkAccessManager>
-#include <QNetworkRequest>
-#include <QNetworkReply>
-
-
+#include "apimanager.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    APIManager apiManager;
+    apiManager.signUp("matiiiiiiinTEST", "1234");
     return a.exec();
 }
